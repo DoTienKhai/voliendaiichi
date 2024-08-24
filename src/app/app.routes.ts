6634e8b45ea85c9ead476a5c72e5532755181831
Khai <dotienkhai06@gmail.com>
2024-08-24 16:49:41 +0700

    add home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'volien-daiichi', pathMatch: 'full'},
+  {path: 'volien-daiichi', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)}
+];
