a4f4eb9dcdb4a54519267bab2a1441c4274afffb
Khai <dotienkhai06@gmail.com>
2024-08-24 18:58:16 +0700

    add css home, image. carousel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage, NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    NgStyle
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  public lsCategories: {name: string, id: string}[] = [
+    {name: 'Trang chủ', id: 'home'},
+    {name: 'Sản phẩm', id: 'product'},
+    {name: 'Feedback', id: 'feedback'},
+    {name: 'Đăng ký', id: 'register'},
+    {name: 'Liên hệ', id: 'contact'},
+  ];
 }
